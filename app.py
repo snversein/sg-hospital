0770bbb3cabb8bf88477b1318a54643da94bed7f
nikhildubey-23 <ravannick0089@gmail.com>
2026-04-18 14:57:10 +0530

    Add dynamic sitemap.xml and robots.txt routes in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request, send_from_directory
+from flask import Flask, render_template, jsonify, request, send_from_directory, Response
 from flask_cors import CORS
 from models import db
 from routes import create_blueprints
@@ -35,13 +35,84 @@ def create_app(config_name=None):
 
     @app.route('/sitemap.xml')
     def sitemap():
-        static_dir = app.static_folder or 'static'
-        return send_from_directory(static_dir, 'sitemap.xml', mimetype='application/xml')
+        sitemap_xml = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://drsushmitaayurveda.in/</loc>
+        <lastmod>2026-04-18</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>https://drsushmitaayurveda.in/#about</loc>
+        <lastmod>2026-04-18</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>https://drsushmitaayurveda.in/#services</loc>
+        <lastmod>2026-04-18</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>https://drsushmitaayurveda.in/#testimonials</loc>
+        <lastmod>2026-04-18</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.6</priority>
+    </url>
+    <url>
+        <loc>https://drsushmitaayurveda.in/#contact</loc>
+        <lastmod>2026-04-18</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>https://drsushmitaayurveda.in/admin</loc>
+        <lastmod>2026-04-18</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.5</priority>
+    </url>
+    <url>
+        <loc>https://drsushmitaayurveda.in/admin/dashboard</loc>
+        <lastmod>2026-04-18</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://drsushmitaayurveda.in/admin/appointments</loc>
+        <lastmod>2026-04-18</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>https://drsushmitaayurveda.in/admin/messages</loc>
+        <lastmod>2026-04-18</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>https://drsushmitaayurveda.in/admin/patients</loc>
+        <lastmod>2026-04-18</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://drsushmitaayurveda.in/api/contact</loc>
+        <lastmod>2026-04-18</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.3</priority>
+    </url>
+</urlset>"""
+        return Response(sitemap_xml, mimetype='application/xml')
 
     @app.route('/robots.txt')
     def robots():
-        static_dir = app.static_folder or 'static'
-        return send_from_directory(static_dir, 'robots.txt', mimetype='text/plain')
+        robots_txt = """User-agent: *
+Allow: /
+
+Sitemap: https://drsushmitaayurveda.in/sitemap.xml"""
+        return Response(robots_txt, mimetype='text/plain')
 
     @app.errorhandler(404)
     def not_found(error):
